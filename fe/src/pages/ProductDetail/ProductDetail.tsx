130f074dc58cdb5ed1dd8efeffa8f66b89ad3913
vy <luongphamgiavy@gmail.com>
2022-05-03 08:27:06 +0700

    sadf

--- fe/src/pages/ProductDetail/ProductDetail.tsx
+++ fe/src/pages/ProductDetail/ProductDetail.tsx
@@ -1,3 +1,4 @@
+import { Image } from "antd";
 import { FC, useEffect, useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,11 +12,12 @@ interface ProductDetail {
 }
 const ProductDetail: FC<ProductDetail> = ({ pageData }) => {
     const dispatch = useDispatch()
-    const cart = useSelector((state:any) => state.cart)
+    const cart = useSelector((state: any) => state.cart)
+    const [Visible, setVisible] = useState(false)
     useEffect(() => {
         console.log(cart);
-    },[cart])
-    
+    }, [cart])
+
     const [Quantity, setQuantity] = useState(1)
     const [MainImage, setMainImage] = useState(pageData.thumbnail)
     const handleQuantity = (e?: any, add?: number) => {
@@ -32,17 +34,19 @@ const ProductDetail: FC<ProductDetail> = ({ pageData }) => {
 
     const handleAddToCart = () => {
         const isHaveInCart = cart.find(o => o._id === pageData._id)
-        if(isHaveInCart) {
+        if (isHaveInCart) {
             isHaveInCart.quantity += Quantity
-        }else {
+        } else {
             cart.push({
                 ...pageData,
-                quantity : Quantity
+                quantity: Quantity
             })
         }
         dispatch(actionChangeCart([...cart]))
         toast(`Đã thêm ${Quantity} sản phẩm vào giỏ hàng`)
     }
+    console.log(pageData?.images);
+    
     return (
         <>
             <div className="product-detail">
@@ -51,16 +55,28 @@ const ProductDetail: FC<ProductDetail> = ({ pageData }) => {
                         <div className="col-4 md-col-6 sm-col-12">
                             <div className="image-slice">
                                 <div className="main">
-                                    <img src={MainImage} alt="" />
+                                    <Image
+                                        preview={{ visible: false }}
+                                        src={MainImage}
+                                        onClick={() => setVisible(true)}
+                                    />
+                                    <div className="hidden-zoom" style={{ display: 'none' }}>
+                                        <Image.PreviewGroup preview={{ visible: Visible, onVisibleChange: vis => setVisible(vis) }}>
+                                            <Image src={pageData.thumbnail} />
+                                            {
+                                                pageData.images?.map((o,index) => <Image key={o + index} src={o} />)
+                                            }
+                                        </Image.PreviewGroup>
+                                    </div>
                                 </div>
                                 <div className="slider">
 
                                     <Slider infinite={false} speed={500} slidesToShow={3} slidesToScroll={3}>
-                                        <div onClick={()=>setMainImage(pageData.thumbnail)} className="img">
+                                        <div onClick={() => setMainImage(pageData.thumbnail)} className="img">
                                             <img src={pageData.thumbnail} alt="" />
                                         </div>
                                         {
-                                            pageData.images?.map((o, index )=> <div onClick={()=>setMainImage(o)} key={o + index} className="img">
+                                            pageData.images?.map((o, index) => <div onClick={() => setMainImage(o)} key={o + index} className="img">
                                                 <img src={o} alt="" />
                                             </div>)
                                         }
@@ -94,7 +110,7 @@ const ProductDetail: FC<ProductDetail> = ({ pageData }) => {
                             </div>
                         </div>
                         <div className="col-12">
-                            <div dangerouslySetInnerHTML={{__html : pageData.content}} className="content"></div>
+                            <div dangerouslySetInnerHTML={{ __html: pageData.content }} className="content"></div>
                         </div>
                     </div>
                 </div>
